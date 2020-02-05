29927f5ec4f2b35742ad3e3d25b5aaa50febe138
sahernandezr <[E50846950+sahernandezr@users.noreply.github.comgit config --global user.email 50846950+sahernandezr@users.noreply.github.com>
2020-02-05 05:48:50 -0600

    Updates

---
+++ precache-manifest.83f984663dd6451499a83c1d67892156.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abc5d8a5222ba5eb15102b5ddb424c72",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "8ede9c8a23ed912a7af5",
+    "url": "/clicky-game/static/css/main.e59d4571.chunk.css"
+  },
+  {
+    "revision": "096096a05405cff2a786",
+    "url": "/clicky-game/static/js/2.3b555bf2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.3b555bf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ede9c8a23ed912a7af5",
+    "url": "/clicky-game/static/js/main.9903572c.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
